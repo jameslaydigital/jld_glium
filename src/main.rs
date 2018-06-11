53736cbc9cac1ca9b525c2f4553db0ba7c9a8e92
James Lay <jameslaymusic@gmail.com>
2018-06-10 20:05:56 -0600

    finished tutorial 8: gouraud shading

--- src/main.rs
+++ src/main.rs
@@ -49,14 +49,17 @@ fn main() {
 
     // BUILD SHADERS {{{
     let vertex_shader_src = r#"
-        #version 140
+        #version 150
 
         in vec3 position;
         in vec3 normal;
 
+        out vec3 v_normal;
+
         uniform mat4 matrix;
 
         void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
             gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
@@ -64,12 +67,15 @@ fn main() {
     let fragment_shader_src = r#"
         #version 140
 
+        in vec3 v_normal;
         out vec4 color;
-
-        uniform sampler2D tex;
+        uniform vec3 u_light;
 
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
 
@@ -102,7 +108,11 @@ fn main() {
             [0.0, 0.0, 0.0, 1.0f32],
         ];
 
-        let uniforms = uniform! { matrix: matrix, };
+        let light = [-1.0, 0.4, 0.9f32];
+        let uniforms = uniform! {
+            u_light: light,
+            matrix: matrix,
+        };
 
         target.draw(
             (&vertices, &normals),
